e1be4197288f26ed7de6204c78f7944963e4b402
Cam <camherringshaw@gmail.com>
2018-11-17 22:17:06 -0600

    Set up example repsonse

--- app.py
+++ app.py
@@ -1,11 +1,41 @@
-from flask import *
+from flask import Flask
+from flask import request
+from secrets import FB_VERIFY_TOKEN
+from secrets import FB_ACCESS_TOKEN
+from pymessenger.bot import Bot
 
 app = Flask(__name__)
+bot = Bot(ACCESS_TOKEN)
+
+
+def verify_fb_token(token_sent):
+    if token_sent == VERIFY_TOKEN:
+        return request.args.get('hub.challenge')
+    return 'Invalid verification token'
+
+def send_message(recipient_id, response):
+    bot.send_text_message(recipient_id, response)
+    return "success"
+
+def get_message():
+    return "Hi there!"
 
 @app.route("/", methods=['GET', 'POST'])
-def index():
-    # return render_template("index.html")
-    return "Hello World"
+def receive_message():
+    if request.method == 'GET':
+        token_sent = request.args.get('hub.verify_token')
+        return verify_fb_token(token_sent)
+    else:
+        output = request.get_json()
+        for event in output['entry']:
+            messaging = event['messaging']:
+            for message in messaging:
+                if message.get('message'):
+                    recipient_id = message['sender']['id']
+                    if message['message'].get('text'):
+                        response_sent_text = get_message()
+                        send_message(recipient_id, response_sent_text)
+        return "Message processed"
 
 
 if __name__ == '__main__':
